229da1094b7af5f7a6a448470f21497b93d020f7
cremans <88071871+cremans@users.noreply.github.com>
2021-10-20 16:04:18 +0000

    Add files via upload

---
+++ revr_start.bat
@@ -0,0 +1,6 @@
+@echo off
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+ping -n 10 127.0.0.1 >nul
